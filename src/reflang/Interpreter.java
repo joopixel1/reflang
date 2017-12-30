3dde52f71b72ea7890aa88f5c6c0b8c1dff3ccf4
Hridesh Rajan <hridesh@gmail.com>
2017-12-30 13:49:40 -0600

    more robust frontend

--- src/reflang/Interpreter.java
+++ src/reflang/Interpreter.java
@@ -1,6 +1,7 @@
 package reflang;
 import java.io.IOException;
 
+import reflang.Env;
 import reflang.AST.*;
 
 /**
@@ -22,18 +23,20 @@ public class Interpreter {
 		Reader reader = new Reader();
 		Evaluator eval = new Evaluator(reader);
 		Printer printer = new Printer();
-		try {
-			while (true) { // Read-Eval-Print-Loop (also known as REPL)
-				Program p = reader.read();
-				try {
-					Value val = eval.valueOf(p);
-					printer.print(val);
-				} catch (Env.LookupException e) {
-					printer.print(e);
-				}
+		REPL: while (true) { // Read-Eval-Print-Loop (also known as REPL)
+			Program p = null;
+			try {
+				p = reader.read();
+				if(p._e == null) continue REPL;
+				Value val = eval.valueOf(p);
+				printer.print(val);
+			} catch (Env.LookupException e) {
+				printer.print(e);
+			} catch (IOException e) {
+				System.out.println("Error reading input:" + e.getMessage());
+			} catch (NullPointerException e) {
+				System.out.println("Error:" + e.getMessage());
 			}
-		} catch (IOException e) {
-			System.out.println("Error reading input.");
 		}
 	}
 }
